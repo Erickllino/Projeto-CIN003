6cbdd6c26984483749d0177e24e9b93e5b8cc6dd
Erick <erickvrc@outlook.com>
2025-03-16 07:37:34 -0300

    feat: Troca para potenciometro

---
+++ jokenpo/adicionalSounds.cpp
@@ -0,0 +1,17 @@
+#include "adicionalSounds.h"
+#include <Arduino.h>
+
+
+// Funções para reagir ao resultado
+void mao_vencedora(){
+    // Vamos colocar aqui os sons e as luzes de que a mão venceu
+  }
+  
+  void mao_derrotada(){
+    // Vamos colocar aqui os sons e as luzes de que a mão perdeu
+  }
+  
+  void mao_empatada(){
+    // Vamos colocar aqui os sons e as luzes de que a mão de empate
+  }
+  
